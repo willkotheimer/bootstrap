84fda44bb7ba8e121df19beb75615a3d35d4ac4b
Will Kotheimer <wkotheimer@gmail.com>
2019-07-26 22:47:51 -0500

    Added counter app - initial commit

---
+++ Mosh-React/counter-app/src/components/counter.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    state = {}
+    render() {
+        return <h1>Hello World</h1>
+    }
+
+}
+
+export default Counter;
