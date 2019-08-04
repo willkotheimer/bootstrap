28150fecaa42ce172801bfd81ad410ba05bb7323
Will Kotheimer <wkotheimer@gmail.com>
2019-08-04 00:44:42 -0500

    Added navbar and lifted state to App component

--- Mosh-React/counter-app/src/components/counters.jsx
+++ Mosh-React/counter-app/src/components/counters.jsx
@@ -2,51 +2,22 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 4 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-      { id: 5, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-  handleReset = () => {
-    const counters = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters });
-  };
-  handleDelete = counterId => {
-    console.log("Event Handler Called", counterId);
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
   render() {
     return (
       <div>
         <button
           onClick={() => {
-            this.handleReset();
+            this.props.onReset();
           }}
           className="btm-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={() => this.handleDelete(counter.id)}
-            onIncrement={this.handleIncrement}
+            onDelete={() => this.props.onDelete(counter.id)}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
