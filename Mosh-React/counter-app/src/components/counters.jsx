44d9242f1ebd00f6032962eb33897fef64196971
Will Kotheimer <wkotheimer@gmail.com>
2019-08-05 12:56:25 -0500

    stateless functional component & object destructuring

--- Mosh-React/counter-app/src/components/counters.jsx
+++ Mosh-React/counter-app/src/components/counters.jsx
@@ -3,21 +3,23 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    //use object destructuring to get this.props
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
         <button
           onClick={() => {
-            this.props.onReset();
+            onReset();
           }}
           className="btm-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={() => this.props.onDelete(counter.id)}
-            onIncrement={this.props.onIncrement}
+            onDelete={() => onDelete(counter.id)}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
--- Mosh-React/counter-app/src/components/navbar.jsx
+++ Mosh-React/counter-app/src/components/navbar.jsx
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//Stateless functional component sfc
+
+//object destructuring for totalCounters
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
 export default NavBar;
